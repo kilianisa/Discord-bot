bb7ddc3f238ac7abe9e69ce57b923dfc5ddf6efd
kilianisa <kilianv1122@gmail.com>
2026-03-31 12:18:11 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,18 @@
+import discord
+from discord.ext import commands
+import os
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix="!", intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f"Bot online als {bot.user}")
+
+@bot.command()
+async def ping(ctx):
+    await ctx.send("Pong 🏓")
+
+bot.run(os.getenv("MTQ4ODQ1MzI2MTk5MTIxNTExNQ.GeDO1p.jvNObtOAIcEi7znWQe_avbwSvsnUkbsFK5ZLJ0"))
